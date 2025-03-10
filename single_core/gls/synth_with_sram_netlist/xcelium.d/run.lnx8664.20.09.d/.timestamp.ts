e976740507cc8e19f0f47c2a5dd90ab14129de29
Shaurya Chopra <shchopra@ucsd.edu>
2025-03-10 13:13:45 -0700

    first run of single core GLS with synth netlist - gives elaboration errors

---
+++ single_core/gls/synth_with_sram_netlist/xcelium.d/run.lnx8664.20.09.d/.timestamp.ts
@@ -0,0 +1,4 @@
+1737007846 /home/linux/ieng6/ee260bwi25/public/PDKdata/verilog/tcbn65gplus.v
+1741464551 /home/linux/ieng6/ee260bwi25/shchopra/project/single_core/rtl/verilog/fullchip.v
+1741468741 /home/linux/ieng6/ee260bwi25/shchopra/project/single_core/rtl/verilog/fullchip_tb.v
+1741473207 /home/linux/ieng6/ee260bwi25/shchopra/project/single_core/synth/synth_with_sram/core.out.v
