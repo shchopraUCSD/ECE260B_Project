5e5f3cdfb236f2b18f30a4007a253f56544484e4
Mayank Kumar <mayank.kumar1123@gmail.com>
2025-03-21 08:16:26 -0700

    Minor updates to floorplan to flip core2)

--- dual_core/pnr/dual_core/initialFloorplan.tcl
+++ dual_core/pnr/dual_core/initialFloorplan.tcl
@@ -1,6 +1,8 @@
 # Floorplan
 floorPlan -site core -r 1 0.50 10.0 10.0 10.0 10.0
 
+#resizeFP -xPercent +200 -yPercent +200
+
 globalNetConnect VDD -type pgpin -pin VDD -inst * -verbose
 globalNetConnect VSS -type pgpin -pin VSS -inst * -verbose
 
@@ -12,11 +14,10 @@ addRing -spacing {top 1 bottom 1 left 1 right 1} -width {top 2 bottom 2 left 2 r
 # note that power stripes in SRAMs are VERTICAL
 addStripe -skip_via_on_wire_shape Noshape -block_ring_top_layer_limit M1 -max_same_layer_jog_length 0.8 -padcore_ring_bottom_layer_limit M1 -number_of_sets 25 -ybottom_offset 5 -skip_via_on_pin Standardcell -stacked_via_top_layer M8 -padcore_ring_top_layer_limit M1 -spacing 1 -merge_stripes_value 0.1 -direction horizontal -layer M5 -block_ring_bottom_layer_limit M1 -ytop_offset 5 -width 1 -area {} -nets {VDD VSS} -stacked_via_bottom_layer M1
 
-setObjFPlanBox Instance core_instance_1 50 280 166.4 395.6
-setObjFPlanBox Instance core_instance_2 50 135 166.4 250.6
+setObjFPlanBox Instance core_instance_1 45.5605 29.525 475.885 459.62
+setObjFPlanBox Instance core_instance_2 655.9895 29.525 1086.531 459.62
 
-flipOrRotateObject -rotate R270 -name core_instance_1
-flipOrRotateObject -rotate R270 -name core_instance_2
+flipOrRotateObject -flip MY -name core_instance_2
 
 #createRouteBlk -exceptpgnet -cover -inst qmem_instance -layer {M1 M2 M3 M4}
 #createRouteBlk -exceptpgnet -cover -inst kmem_instance -layer {M1 M2 M3 M4}
